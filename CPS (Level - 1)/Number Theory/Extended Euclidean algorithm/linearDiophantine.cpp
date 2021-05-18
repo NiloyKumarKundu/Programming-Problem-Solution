c713bb52e275c274a2c2f548d38ed77461345fae
Niloy <nkundu191071@bscse.uiu.ac.bd>
2021-05-19 01:41:15 +0600

    Problem Solving

---
+++ CPS (Level - 1)/Number Theory/Extended Euclidean algorithm/linearDiophantine.cpp
@@ -0,0 +1,140 @@
+#include <bits/stdc++.h>
+#define Niloy
+#define int int64_t
+#define mx (int) 1e5 + 123
+#define MOD (int) 1e9 + 7
+#define pb push_back
+#define pairs pair<int, int>
+#define vi vector<int>
+#define vb vector<bool>
+#define vii vector<pairs>
+#define lb lower_bound
+#define ub upper_bound
+#define endl '\n'
+#define llu unsigned long long
+using namespace std;
+/* ----------------------------------------------------------------------------------- */
+
+// Input/Output
+#define fastInput ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#define all(x) x.begin(), x.end()
+#define square(a) (a * a)
+#define mem(a, b) memset(a, b, sizeof(a))
+
+// Fractional Number
+#define fraction()        cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed, ios::floatfield);
+
+// Direction Array
+int dx[] = {0, 0, 1, -1, 1, 1, -1, -1};
+int dy[] = {1, -1, 0, 0, 1, -1, 1, -1};
+
+// File I/O
+#define read(x)	 freopen(x, "r", stdin);
+#define write(x) freopen(x, "w", stdout);
+ 
+// Loops
+#define rep(i, a, n) for (int i = a; i < n; i++)
+#define REP(i, a, n) for (int i = a; i <= n; i++)
+#define rev(i, n, a) for (int i = n - 1; i >= a; i--)
+#define REV(i, n, a) for (int i = n; i >= a; i--)
+ 
+/* ----------------------------------------------------------------------------------- */
+ 
+#define Cases  cout << "Case " << ++Case << ": ";
+#define __test int tt; int Case=0; cin >> tt; while(tt--)
+#define showTime cerr << "time = " << (clock() / CLOCKS_PER_SEC) << " sec" << '\n';
+ 
+#define dbgA2(A, n, m) {cout<<"--> "<<#A<<" = \n";rep(i, 0, n){rep(j, 0, m){cout<<A[i][j]<<"";}cout<<"\n";}cout<<"\n";}
+#define dbgA(A, n) {cout<<" --> "<<#A<<" = (";rep(i, 0, n)cout<<A[i]<<" ";cout<<")\n";}
+#define dbg(args...) {string sss(#args);sss+=',';cout<<" --> ";debugger::call(all(sss), args);cout<<"\n";}
+ 
+/* ----------------------------------------------------------------------------------- */
+ 
+int gcd(int n, int m) { return m ? gcd(m, n % m) : n; }
+int lcm(int n, int m) { return n / gcd(n, m) * m; }
+ 
+struct debugger {
+    typedef string::iterator si;
+    static void call(si it, si ed) {}
+    template<typename T, typename ... aT>
+    static void call(si it, si ed, T a, aT... rest) {
+        string b;
+        for(; *it!=','; ++it)
+            if(*it!=' ')
+                b+=*it;
+        cout << b << "=" << a << " ";
+        call(++it, ed, rest...);
+    }
+};
+
+/* ----------------------------------------------------------------------------------- */
+void input() {
+#ifdef Niloy
+    read("input.txt");  
+    write("output.txt");
+#endif
+}
+
+/* ----------------------------------------------------------------------------------- */
+
+int extendedEuclidean(int A, int B, int *X, int *Y) {
+	int x, y, x1, y1, x2, y2, q, r, r1, r2;
+	x1 = 0, y1 = 1;
+	x2 = 1, y2 = 0;
+
+	for (r2 = A, r1 = B; r1 != 0; r2 = r1, r1 = r, x2 = x1, y2 = y1, x1 = x, y1 = y) {
+		q = r2 / r1;
+		r = r2 % r1;
+		x = x2 - (q * x1);
+		y = y2 - (q * y1);
+	}
+	*X = x2;
+	*Y = y2;
+	return r2;
+}
+
+bool linearDiophantine(int A, int B, int C, int *x, int *y) {
+	int g = gcd(A, B);
+    if (C % g != 0) {
+		return false;       // No solution
+	}
+
+	int a = A / g, b = B / g, c = C / g;
+	extendedEuclidean(a, b, x, y);	 // Solve ax + by = 1
+
+	if (g < 0) {   // Make sure gcd(a, b) = 1 OR gcd is always positive
+		a *= -1, b *= -1, c *= -1;
+	}
+	*x *= c, *y *= c;	// ax + by = c;
+	return true;		// Solution exist
+}
+
+void solve() {
+	int a, b, c, x, y;
+	cin >> a >> b >> c;
+	int res = linearDiophantine(a, b, c, &x, &y);
+    if (!res) {
+		cout << "No solution\n";
+		return;
+	}
+    cout << "One possible solution (" << x << ", " << y << ")\n";
+    int g = gcd(a, b);
+    int k = 1;
+	cout << "Another possible solution (" << x + k * (b / g) << ", " << y - k * (a / g) << ")\n";
+	k = 2;
+	cout << "Another possible solution (" << x + k * (b / g) << ", " << y - k * (a / g) << ")\n";
+	// we can use k = -infinity to +infinity.
+}
+
+int32_t main() {
+    // input();
+    fastInput;
+    solve();
+
+    // __test {
+    // 	solve();
+    // }
+
+    // showTime;
+    return 0;
+}
