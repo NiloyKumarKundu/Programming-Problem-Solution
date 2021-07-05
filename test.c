d1d483d5a5561ce82c3229772aaa41b5e6315726
Niloy <nkundu191071@bscse.uiu.ac.bd>
2021-07-05 16:54:59 +0600

    Problem Solving

--- test.c
+++ test.c
@@ -1,16 +1,7 @@
 #include <stdio.h>
 
 int main() {
-	int numOne, numTwo;
-	printf("Enter two number\n");
-	scanf("%d %d", &numOne, &numTwo);
 
-	printf("Addition: %d\n", numOne + numTwo);
-	printf("Subtraction: %d\n", numOne - numTwo);
-	printf("Multiplication: %d\n", numOne * numTwo);
-    if (numTwo == 0) {
-		printf("Divide by zero is not possible!!\n");
-	} else {
-		printf("Division: %.2lf\n", (numOne * 1.0) / numTwo);
-	}
+	
+	return 0;
 }
