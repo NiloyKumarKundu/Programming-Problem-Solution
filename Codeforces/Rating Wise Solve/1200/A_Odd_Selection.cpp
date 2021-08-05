cbca1888bb9229cae774f5e33b038fbe02b4bc68
Niloy <nkundu191071@bscse.uiu.ac.bd>
2021-08-05 10:33:24 +0600

    Problem Solving

---
+++ Codeforces/Rating Wise Solve/1200/A_Odd_Selection.cpp
@@ -0,0 +1,141 @@
+#include <bits/stdc++.h>
+#define Niloy
+#define int int64_t
+#define mx (int) 1e5 + 123
+#define MOD (int) 1e9 + 7
+#define pb push_back
+#define pairs pair<int, int>
+#define vi vector<int>
+#define vb vector<bool>
+#define vii vector<pairs>
+#define lb lower_bound
+#define ub upper_bound
+#define endl '\n'
+#define llu unsigned long long
+using namespace std;
+/* ----------------------------------------------------------------------------------- */
+
+// Input/Output
+#define fastInput ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#define all(x) x.begin(), x.end()
+#define square(a) (a * a)
+#define mem(a, b) memset(a, b, sizeof(a))
+
+// Fractional Number
+#define fraction()        cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed, ios::floatfield);
+
+// Direction Array
+int dx[] = {0, 0, 1, -1, 1, 1, -1, -1};
+int dy[] = {1, -1, 0, 0, 1, -1, 1, -1};
+
+// File I/O
+#define read(x)	 freopen(x, "r", stdin);
+#define write(x) freopen(x, "w", stdout);
+ 
+// Loops
+#define rep(i, a, n) for (int i = a; i < n; i++)
+#define REP(i, a, n) for (int i = a; i <= n; i++)
+#define rev(i, n, a) for (int i = n - 1; i >= a; i--)
+#define REV(i, n, a) for (int i = n; i >= a; i--)
+ 
+/* ----------------------------------------------------------------------------------- */
+ 
+#define Cases  cout << "Case " << ++Case << ": ";
+#define __test int tt; int Case=0; cin >> tt; while(tt--)
+#define showTime cerr << "time = " << (clock() / CLOCKS_PER_SEC) << " sec" << '\n';
+ 
+#define dbgA2(A, n, m) {cout<<"--> "<<#A<<" = \n";rep(i, 0, n){rep(j, 0, m){cout<<A[i][j]<<"";}cout<<"\n";}cout<<"\n";}
+#define dbgA(A, n) {cout<<" --> "<<#A<<" = (";rep(i, 0, n)cout<<A[i]<<" ";cout<<")\n";}
+#define dbg(args...) {string sss(#args);sss+=',';cout<<" --> ";debugger::call(all(sss), args);cout<<"\n";}
+ 
+/* ----------------------------------------------------------------------------------- */
+ 
+int gcd(int n, int m) { return m ? gcd(m, n % m) : n; }
+int lcm(int n, int m) { return n / gcd(n, m) * m; }
+ 
+struct debugger {
+    typedef string::iterator si;
+    static void call(si it, si ed) {}
+    template<typename T, typename ... aT>
+    static void call(si it, si ed, T a, aT... rest) {
+        string b;
+        for(; *it!=','; ++it)
+            if(*it!=' ')
+                b+=*it;
+        cout << b << "=" << a << " ";
+        call(++it, ed, rest...);
+    }
+};
+
+/* ----------------------------------------------------------------------------------- */
+void input() {
+#ifdef Niloy
+    read("input.txt");  
+    write("output.txt");
+#endif
+}
+
+/* ----------------------------------------------------------------------------------- */
+
+void solve() {
+	int n, k;
+	cin >> n >> k;
+	int arr[n], even = 0, odd = 0;
+
+    rep(i, 0, n) {
+		cin >> arr[i];
+        if (arr[i] % 2 == 0) {
+			even++;
+		} else {
+			odd++;
+		}
+	}
+
+    if (odd == 0) {
+		cout << "No\n";
+		return;
+	}
+    if (even == 0) {
+        if (k % 2 == 0) {
+			cout << "No\n";
+			return;
+		}
+        if (odd % 2 && odd >= k) {
+			cout << "Yes\n";
+		} else {
+            if (odd - 1 >= k) {
+				cout << "Yes\n";
+			} else {
+				cout << "No\n";
+			}
+		}
+		return;
+	}
+
+    if (odd % 2) {
+        if (odd + even >= k) {
+			cout << "Yes\n";
+		} else {
+			cout << "No\n";
+		}
+	} else {
+        if (odd + even - 1 >= k) {
+			cout << "Yes\n";
+		} else {
+			cout << "No\n";
+		}
+	}
+}
+
+int32_t main() {
+    // input();
+    fastInput;
+    // solve();
+
+    __test {
+    	solve();
+    }
+
+    // showTime;
+    return 0;
+}
