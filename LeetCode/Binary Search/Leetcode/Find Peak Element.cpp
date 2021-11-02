a226fc4c3a36088c0ddb360355bff52e4703bfb5
Niloy <nkundu191071@bscse.uiu.ac.bd>
2021-11-03 01:34:53 +0600

    Problem Solving

---
+++ LeetCode/Binary Search/Leetcode/Find Peak Element.cpp
@@ -0,0 +1,66 @@
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int low = 0, high = nums.size() - 1;
+        if (nums.size() == 1) {
+            return 0;
+        }
+        
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            
+            if ((mid > 0) && (mid < nums.size() - 1)) {
+                if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]) {
+                    return mid;
+                } else if (nums[mid - 1] > nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } else if (mid == 0) {
+                if (nums[mid] > nums[mid + 1]) {
+                    return 0;
+                } else {
+                    return 1;
+                }
+            } else if (mid == nums.size() - 1) {
+                if (nums[nums.size() - 1] > nums[nums.size() - 2]) {
+                    return nums.size() - 1;
+                } else {
+                    return nums.size() - 2;
+                }
+            }
+        }
+        return 0;
+    }
+};
+
+
+
+
+
+
+
+
+
+// Another way...
+
+
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int low = 0, high = nums.size() - 1;
+        
+        while (low < high) {
+            int mid1 = low + (high - low) / 2;
+            int mid2 = mid1 + 1;
+            
+            if (nums[mid1] < nums[mid2]) {
+                low = mid2;
+            } else {
+                high = mid1;
+            }
+        }
+        return low;
+    }
+};
